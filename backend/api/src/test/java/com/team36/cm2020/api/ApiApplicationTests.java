4f0834468db19f5c148148a483604e0cfd123871
Ty Irvine <39813066+tyirvine@users.noreply.github.com>
2025-01-30 12:25:18 -0700

    Set up simple backend and frontend structure
    As well, I created two Spring starter applications in the backend.

---
+++ backend/api/src/test/java/com/team36/cm2020/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team36.cm2020.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

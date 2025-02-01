c2a05293a3c3d5f1a4885468684d1e44416fe34b
Гоосен Екатерина Андреевна <boldina.ea@esphere.ru>
2025-02-01 18:31:48 +0300

    initial commit

---
+++ backend/notifications-service/src/main/java/com/team_36/cm2020/notifications_service/NotificationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.team_36.cm2020.notifications_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationsServiceApplication.class, args);
+	}
+
+}

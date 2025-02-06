4b7ee0126258d930617a703754eeffe911b71990
nachocontreres <ignacio@loopmein.me>
2025-02-05 23:22:27 -0300

    configured tailwind and shadcdn, started home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,37 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/shadcdn/ui/card";
+
 function App() {
   return (
     <div>
-      <h1>Syncify!</h1>
+      <div className='bg-primary h-[80px] w-full pl-[50px] flex flex-row items-center'>
+        <p className='text-black font-semibold text-2xl'>Syncify</p>
+      </div>
+      <div className='pt-[100px] px-[50px] flex flex-col'>
+        <div className='flex flex-row gap-[50px]'>
+          <Card className='w-[350px] bg-primary pb-5 cursor-pointer'>
+            <CardHeader>
+              <CardTitle className='text-xl'>Schedule meeting</CardTitle>
+              <CardDescription>
+                Create a meeting task by specifying the duration and date range
+                for scheduling.
+              </CardDescription>
+            </CardHeader>
+          </Card>
+          <Card className='w-[350px] bg-primary pb-5 cursor-pointer'>
+            <CardHeader>
+              <CardTitle className='text-xl'>View Scheduled Meetings</CardTitle>
+              <CardDescription>
+                Access a list of all scheduled meetings with their details.
+              </CardDescription>
+            </CardHeader>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 }
